8c1424a16bafd8c67ec6f11dd60068eb30ed14ae
firepick1 (localhost) <karl@firepick.org>
2016-10-24 22:58:32 -0700

    --json

--- bin/jspcb.js
+++ bin/jspcb.js
@@ -15,40 +15,46 @@ const help = [
     "	jspcb {OPTIONS}",
     "",
     "OPTIONS",
-    "    -v --version",
-    "        Print version number",
-    "    --verbose",
-    "        Print verbose output",
-    "    -h --help",
-    "        Print these instructions",
-    "    --csv-holes PATH",
-    "        Generate CSV file for PCB holes",
-    "    --csv-smdpads PATH",
-    "        Generate CSV file for PCB SMD pads",
-    "    --svg PATH",
-    "        Generate SVG file",
-    "    --png PATH",
-    "        Generate PNG file",
-    "    --gbl PATH",
-    "        Read Gerber bottom copper file",
-    "    --gbo PATH",
-    "        Read Gerber bottom silkscreen file",
-    "    --gbs PATH",
-    "        Read Gerber bottom soldermask file",
-    "    --gko PATH",
-    "        Read Gerber keepout file (Altium/Protel board outline)",
-    "    --gml PATH",
-    "        Read Gerber mill file",
-    "    --gtl PATH",
-    "        Read Gerber top copper file",
-    "    --gto PATH",
-    "        Read Gerber top silkscreen file",
-    "    --gtp PATH",
-    "        Read Gerber top paste file",
-    "    --gts PATH",
-    "        Read Gerber top soldermask file",
-    "    --txt PATH",
-    "        Read Gerber drill file",
+    "\t-v --version",
+    "\t\tPrint version number",
+    "\t--verbose",
+    "\t\tPrint verbose output",
+    "\t-j --json TRANSFORMFILE",
+    "\t\tRun transformations specified in given JSON file",
+    "\t-h --help",
+    "\t\tPrint these instructions",
+    "\t--csv-holes PATH",
+    "\t\tGenerate CSV file for PCB holes",
+    "\t--csv-smdpads PATH",
+    "\t\tGenerate CSV file for PCB SMD pads",
+    "\t--svg PATH",
+    "\t\tGenerate SVG file",
+    "\t--png PATH",
+    "\t\tGenerate PNG file",
+    "\t--png-height PIXELS",
+    "\t\tSpecify PNG image height",
+    "\t--png-width PIXELS",
+    "\t\tSpecify PNG image width",
+    "\t--gbl PATH",
+    "\t\tRead Gerber bottom copper file",
+    "\t--gbo PATH",
+    "\t\tRead Gerber bottom silkscreen file",
+    "\t--gbs PATH",
+    "\t\tRead Gerber bottom soldermask file",
+    "\t--gko PATH",
+    "\t\tRead Gerber keepout file (Altium/Protel board outline)",
+    "\t--gml PATH",
+    "\t\tRead Gerber mill file",
+    "\t--gtl PATH",
+    "\t\tRead Gerber top copper file",
+    "\t--gto PATH",
+    "\t\tRead Gerber top silkscreen file",
+    "\t--gtp PATH",
+    "\t\tRead Gerber top paste file",
+    "\t--gts PATH",
+    "\t\tRead Gerber top soldermask file",
+    "\t--txt PATH",
+    "\t\tRead Gerber drill file",
     ""
 ];
 
@@ -59,37 +65,59 @@ const PcbTransform = require("./../lib/pcbtransform");
 const LogFile = require("./../lib/logfile");
 
 (function(exports) {
-    var eagle = {
-        path: null,
-        layer: "Top",
-        show: "SMD",
-    };
-    var gerber = {
-        layers: {},
-    };
-    var output = "CSV";
-    var verbose = false;
 
     function JSPcb(argv) {
         var that = this;
-        that.verbose = false;
-        that.options = {};
         that.pcbx = new PcbTransform();
+        that.xfm = {
+            verbose: false,
+            eagle: { //input
+                path: null,
+                layer: "Top",
+                show: "SMD",
+            },
+            gerber: { //input
+                layers: {},
+            },
+            bounds: { //output
+                l: 0,
+                t: null,
+                r: null,
+                b: 0,
+            },
+            csv: { // output
+            },
+            colors: { // output
+                "board": "#000",
+                "outline": "#000",
+                "pad": "#fff",
+                "text": "#444",
+                "hole": "#000"
+            },
+            png: { // output
+                path: null,
+                width: 800,
+                height: null,
+            },
+            svg: { // output
+                path: null,
+            },
+        };
 
-        that.processArgs(argv);
+        that.processArgs(argv, that.xfm);
 
-        that.verbose && console.log("jspcb command line");
+        that.xfm.verbose && console.log("jspcb command line");
         argv.length <= 2 && process.exit(showHelp());
 
-        if (eagle.path) {
-            that.pcbx.readEagleBrd(eagle.path);
-        } else if (Object.keys(gerber.layers).length) {
-            that.pcbx.readGerber(gerber.layers);
+        if (that.xfm.eagle.path) {
+            that.pcbx.readEagleBrd(that.xfm.eagle.path);
+        } else if (Object.keys(that.xfm.gerber.layers).length) {
+            that.pcbx.readGerber(that.xfm.gerber.layers);
         } else {
             // do nothing
         }
-        if (that.svgFile) {
-            var writer = new LogFile(that.svgFile);
+        if (that.xfm.svg.path) {
+            var writer = new LogFile(that.xfm.svg.path);
             that.pcbx.writeSvg({
                 writer: writer,
                 layers: {
@@ -98,21 +126,21 @@ const LogFile = require("./../lib/logfile");
             });
             writer.close();
         }
-        if (that.csvSmdPads) {
-            var writer = new LogFile(that.csvSmdPads);
+        if (that.xfm.csv.smdpads) {
+            var writer = new LogFile(that.xfm.csv.smdpads);
             that.pcbx.writeCsv({
                 smdpads: writer
             });
             writer.close();
         }
-        if (that.csvHoles) {
-            var writer = new LogFile(that.csvHoles);
+        if (that.xfm.csv.holes) {
+            var writer = new LogFile(that.xfm.csv.holes);
             that.pcbx.writeCsv({
                 holes: writer
             });
             writer.close();
         }
-        (that.png) && that.pcbx.writePng(that.png);
+        (that.xfm.png.path) && that.pcbx.writePng(that.xfm.png);
         return that;
     }
 
@@ -123,23 +151,23 @@ const LogFile = require("./../lib/logfile");
         return 0;
     }
 
-    JSPcb.prototype.parseOptions = function(options) {
+    JSPcb.prototype.parseJsonTransform = function(transform) {
         var that = this;
-        if ((typeof options === "string") || (options instanceof Buffer)) {
-            options = JSON.parse(options);
+        if ((typeof transform === "string") || (transform instanceof Buffer)) {
+            transform = JSON.parse(transform);
         }
-        options = options || {};
-        if (options.bounds && options.bounds.units) {
-            options.bounds.l *= options.bounds.units;
-            options.bounds.t *= options.bounds.units;
-            options.bounds.r *= options.bounds.units;
-            options.bounds.b *= options.bounds.units;
+        transform = transform || {};
+        if (transform.bounds && transform.bounds.units) {
+            transform.bounds.l *= transform.bounds.units;
+            transform.bounds.t *= transform.bounds.units;
+            transform.bounds.r *= transform.bounds.units;
+            transform.bounds.b *= transform.bounds.units;
         }
-        that.options = options;
-        that.verbose && console.log("JSON options:", JSON.stringify(options, null, " "));
+        that.xfm = transform;
+        that.xfm.verbose && console.log("JSON transform:", JSON.stringify(transform, null, " "));
     }
 
-    JSPcb.prototype.processArgs = function(argv) {
+    JSPcb.prototype.processArgs = function(argv, xfm) {
         var that = this;
         for (var iArg = 2; iArg < process.argv.length; iArg++) {
             var arg = argv[iArg];
@@ -149,61 +177,67 @@ const LogFile = require("./../lib/logfile");
                     console.log("JsPCB v" + that.pcbx.version());
                     break;
                 case '--gbl': // bottom copper
-                    gerber.layers.gtl = argv[++iArg];
+                    xfm.gerber.layers.gtl = argv[++iArg];
                     break;
                 case '--gbo': // bottom silkscreen
-                    gerber.layers.gbo = argv[++iArg];
+                    xfm.gerber.layers.gbo = argv[++iArg];
                     break;
                 case '--gbs': // bottom soldermask
-                    gerber.layers.gbs = argv[++iArg];
+                    xfm.gerber.layers.gbs = argv[++iArg];
                     break;
                 case '--gko': // Altima keepout board outline
-                    gerber.layers.gko = argv[++iArg];
+                    xfm.gerber.layers.gko = argv[++iArg];
                     break;
                 case '--gml': // mill
-                    gerber.layers.gml = argv[++iArg];
+                    xfm.gerber.layers.gml = argv[++iArg];
                     break;
                 case '--gtl': // top copper
-                    gerber.layers.gtl = argv[++iArg];
+                    xfm.gerber.layers.gtl = argv[++iArg];
                     break;
                 case '--gto': // top silkscreen
-                    gerber.layers.gto = argv[++iArg];
+                    xfm.gerber.layers.gto = argv[++iArg];
                     break;
                 case '--gtp': // top paste
-                    gerber.layers.gtp = argv[++iArg];
+                    xfm.gerber.layers.gtp = argv[++iArg];
                     break;
                 case '--gts': // top soldermask
-                    gerber.layers.gts = argv[++iArg];
+                    xfm.gerber.layers.gts = argv[++iArg];
                     break;
                 case '--txt': // drill file
-                    gerber.layers.txt = argv[++iArg];
+                    xfm.gerber.layers.txt = argv[++iArg];
                     break;
                 case '--svg': // SVG output file
-                    that.svgFile = argv[++iArg];
+                    xfm.svg.path = argv[++iArg];
                     break;
                 case '--csv-holes': // CSV output file
-                    that.csvHoles = argv[++iArg];
+                    xfm.csv.holes = argv[++iArg];
                     break;
                 case '--csv-smdpads': // CSV output file
-                    that.csvSmdPads = argv[++iArg];
+                    xfm.csv.smdpads = argv[++iArg];
                     break;
                 case '--png': // PNG output file
-                    that.png = argv[++iArg];
+                    xfm.png.path = argv[++iArg];
+                    break;
+                case '--png-height': // PNG output file pixel height
+                    xfm.png.height = Number(argv[++iArg]);
+                    break;
+                case '--png-width': // PNG output file pixel width
+                    xfm.png.width = Number(argv[++iArg]);
                     break;
                 case '--eagle':
-                    eagle.path = argv[++iArg];
+                    xfm.eagle.path = argv[++iArg];
                     break;
                 case '-h':
                 case '--help':
                     process.exit(showHelp());
                     break;
                 case '--verbose':
-                    that.verbose = true;
+                    xfm.verbose = true;
                     break;
                 case '-j':
                 case '--json':
-                    var options = fs.readFileSync(argv[++iArg]);
-                    that.parseOptions(options);
+                    var transform = fs.readFileSync(argv[++iArg]);
+                    that.parseJsonTransform(transform);
                     break;
                 default:
                     console.log("unexpected argument:", arg);
